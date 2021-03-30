39aa098bcf0a48584bbbca26e14d86391812aabe
Aldo Diva Saputra <66655944+AldoDiva@users.noreply.github.com>
2021-03-30 16:52:29 +0700

    Update config.php

--- Minggu 9/application/config/config.php
+++ Minggu 9/application/config/config.php
@@ -1,4 +1,4 @@
-<?php
+O<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/AB/';
+$config['base_url'] = 'http://localhost/Minggu 9/';
 
 /*
 |--------------------------------------------------------------------------
